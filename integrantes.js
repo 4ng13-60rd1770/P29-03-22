44db79793d229b618f3a800ac87b72eef7801e15
julianest <julianest12@gmail.com>
2022-03-29 18:11:10 -0500

    se crea integrantes.js

---
+++ integrantes.js
@@ -0,0 +1,10 @@
+document.getElementById("f10").innerHTML = `<div class="card" style="width: 18rem;">
+<img src="https://scontent.fbog3-1.fna.fbcdn.net/v/t1.18169-9/13221457_10208411732785389_8233495547711985480_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_eui2=AeE-6F62ce7l6NlR47aLjH1rjoFe402ehoeOgV7jTZ6Gh7AaTNyebskVJHJtYOycNOg&_nc_ohc=IO-QiUz_Rm8AX9BVmFq&_nc_ht=scontent.fbog3-1.fna&oh=00_AT8fvjMtzMm75jRV_InHVvdBkap1U2SZTgt3Wo4rIqYWVQ&oe=626852FF" class="card-img-top" alt="Julian H">
+<div class="card-body">
+  <h5 class="card-title">Julian Huerfano</h5>
+  <p class="card-text">No he programado, pero si he venido realizando pequeños cursos, vengo de otra profesion por lo que me gustaria poder llegar a tener mucho conocimiento y sobresalir en este campo</p>
+  <p class="card-text">28 Años de edad</p>
+  <p class="card-text">Bogota D.C.</p>
+
+</div>
+</div>`
